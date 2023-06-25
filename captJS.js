0b8282eac1cc5a746cc6cf334d8ac5f91fa601cd
BALAMURUGAN16SA <136428164+BALAMURUGAN16SA@users.noreply.github.com>
2023-06-25 16:41:02 +0530

    Update captJS.js

--- captJS.js
+++ captJS.js
@@ -1,8 +1,19 @@
-function fun(){
+var cpt = "";
+
+function fun1(){
   var str = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789";
-  var cpt = "";
   for(var i = 0; i < 6; i++){
     cpt += str[Math.floor(Math.random()*(str.length))];
   }
   document.getElementById('id1').innerHTML = cpt;
+}
+
+function fun2() {
+	var inp = document.getElementById('id2').value;
+	if(!(cpt.localeCompare(inp))){
+		alert('VALID');
+	}
+	else{
+		alert('INVALID');
+	}
 }
