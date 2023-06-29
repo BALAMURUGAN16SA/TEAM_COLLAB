6bb0b880af6377519ce1262ad613536e86203e11
harisangar2004 <122169178+harisangar2004@users.noreply.github.com>
2023-06-29 19:42:15 +0530

    Update GAME_PAGE_J.js

--- GAME_PAGE_J.js
+++ GAME_PAGE_J.js
@@ -1,3 +1,21 @@
 function please_work(){
 	document.getElementById('center-box').innerHTML = '<object type = "text/html" data = "TTT1.html" style = "width : 100%; height : 100%;" ></object>';
 }
+
+$(document).ready(function () {
+	$('.fa-bars').click(function () {
+		$(this).toggleClass('fa-times');
+		$('.navbar').toggleClass('nav-toggle');
+	});
+
+	$(window).on('scroll load', function () {
+		$('.fa-bars').removeClass('fa-times');
+		$('.navbar').removeClass('nav-toggle');
+	});
+
+	if ($(window).scrollTop() > 30) {
+		$('header').addClass('header-active');
+	} else {
+		$('header').removeClass('header-active');
+	}
+});
