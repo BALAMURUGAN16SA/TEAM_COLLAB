1e97b92b19212c9bf0f46362fdabdf826e843393
BALAMURUGAN16SA <136428164+BALAMURUGAN16SA@users.noreply.github.com>
2023-07-22 10:35:26 +0530

    C

--- GAME_PAGE_J.js
+++ GAME_PAGE_J.js
@@ -1,3 +1,9 @@
-function please_work(){
+function please_work_u(){
 	document.getElementById('center-box').innerHTML = '<object type = "text/html" data = "TTT1.html" style = "width : 100%; height : 100%;" ></object>';
 }
+
+function please_work_c(){
+	document.getElementById('center-box').innerHTML = '<object type = "text/html" data = "TTT2H.html" style = "width : 100%; height : 100%;" ></object>';
+}
+
+
